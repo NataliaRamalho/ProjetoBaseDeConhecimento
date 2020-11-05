a3b26dfb7f87b7c91f07aa337b1555f03efc15a0
Natália Ramalho <NataliaRamalho@users.noreply.github.com>
2020-11-05 13:33:09 -0300

    Criação e configuração da api de artigos

---
+++ backend/api/article.js
@@ -0,0 +1,77 @@
+module.exports = app =>{
+    const {existsOrError, notExistsOrError} = app.api.validation
+
+    const save= (req,res)=>{
+        const article = {...req.body}
+        if(req.params.id) article.id = req.params.id
+        try{
+           existsOrError(article.name, 'Nome não informado') 
+           existsOrError(article.description, 'Descriçaõ não infromada') 
+           existsOrError(article.categoryId, 'Categoria não informada') 
+           existsOrError(article.userId, 'Autor não informada') 
+           existsOrError(article.content, 'Conteúdo não informada') 
+           
+        }
+        catch(msg){
+            return res.status(400).send(msg)
+        }
+        if(article.id){
+            app.db('articles')
+                .update(article)
+                .where({id: article.id})
+                .then(_=> res.status(204).send())
+                .catch(err => res.status(500).send(err))
+        } else{
+            app.db('articles')
+               .insert(article)
+               .then(_=> res.status(204).send())
+               .catch(err => res.status(500).send(err))
+        }
+    }
+
+    const remove = async(req,res) =>{
+        try{
+            const rowDeleted = await app.db('articles')
+                .where({id:req.params.id}).del()
+            try{
+                existsOrError(rowDeleted, 'Artigo não foi encontrado')
+            }catch(msg){
+               return res.status(500).send(msg)
+            }
+            
+            res.status(204).send()
+        }
+        catch(msg){
+            res.status(500).send(msg)
+        }
+    }
+    
+    //usada para paginação
+    const limit = 10
+
+    const get = async (req, res) =>{
+        const page = req.query.page || 1
+
+        const result = await app.db('articles').count('id').first()
+        const count = parseInt(result.count)
+
+        app.db('articles')
+            .select('id', 'name','description')
+            .limit(limit).offset(page * limit - limit)
+            .then(article => res.json({data: article, count, limit}))
+            .catch(err => res.status(500).send(err))
+        }
+    
+        const getById = (req, res) =>{
+            app.db('articles')
+                .where({id: req.params.id})
+                .first()
+                .then(article => {
+                    article.content = article.content.toString()
+                    return res.json(article)
+                }) 
+                .catch(err => res.status(500).send(err) )   
+        }
+        
+    return{save, remove, get, getById}
+}
