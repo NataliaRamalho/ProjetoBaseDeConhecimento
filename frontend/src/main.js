749940f9958d7ee9ffe4e05ae4dfe441621281ca
Natália Ramalho <NataliaRamalho@users.noreply.github.com>
2020-12-05 15:35:57 -0300

    Configução da autenticação do usuário

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,8 +5,11 @@ import App from './App'
 
 import './config/bootstrap'
 import './config/msgs'
+import './config/axios'
+
 import store from './config/store'
 import router from './config/router'
+
 
 Vue.config.productionTip = false
 
